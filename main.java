66244a2b45c82521aa5951af55aeb082323e9fc4
loh-siqueira <128156957+loh-siqueira@users.noreply.github.com>
2024-10-31 08:15:02 -0300

    .

---
+++ main.java
@@ -0,0 +1,12 @@
+class Helloword {
+   public static void main(String[] args) {
+     System.out.println("Esse e o Screen Match");
+     System.out.println("Filme: Me Chame Pelo Seu Nome");
+    
+    int ano = 2017;
+    boolean incluidoNoPlano = true;
+    double notaFilme = 5.5;
+    
+    System.out.println("ano de lançamento " + ano);
+   }
+}
