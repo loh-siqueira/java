5731bff0297e28a9f7a28946e3cb2a3c51bbc7d2
loh-siqueira <128156957+loh-siqueira@users.noreply.github.com>
2024-11-06 08:58:13 -0300

    .

--- main.java
+++ main.java
@@ -8,5 +8,12 @@ class Helloword {
     double notaFilme = 5.5;
     
     System.out.println("ano de lançamento " + ano);
+
+    double media = (5.5 = 2.6)/2;
+    System.out.println("A media das notas deste filme e: " +media);
+    String sinopse;
+    sinops ="O sensível Elio é o único filho de uma família norte-americana com ascendência italiana e francesa. O garoto está enfrentando outro verão preguiçoso na casa de seus pais na bela paisagem italiana. Nesse cenário, Oliver, acadêmico que veio ajudar a pesquisa de seu pai, chega para despertar sentimentos ainda desconhecidos.";
+    system.out.println(sinopse);
+    
    }
 }
